b712133391f35dbeb11ac7451217a0c353c869a4
mohammed <baigm066@gmail.com>
2024-01-25 11:25:47 +0530

    new push again

--- zaco/api/urls.py
+++ zaco/api/urls.py
@@ -5,5 +5,5 @@ from api import views
 urlpatterns = [
     path('', views.home,name="home"),
     path('view-eosl', views.view_eosl,name="view-eosl"),
-    path('detail-view-eosl', views.detail_view_eosl,name="detail-view-eosl"),
+    path('detail-view-eosl/<str:name>', views.detail_view_eosl,name="detail-view-eosl"),
 ]
